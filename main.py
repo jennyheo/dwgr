c0353716283068f2c847189cc0476e51254c3d8a
jennyheo <hyj3589@gmail.com>
2025-04-08 23:00:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
 import streamlit as st
-
-# [google_sheets]
-# type = "service_account"
-# project_id = "your-project-id"
-# private_key_id = "your-private-key-id"
-# private_key = "-----BEGIN PRIVATE KEY-----\\nYOUR-PRIVATE-KEY\\n-----END PRIVATE KEY-----\\n"
-# client_email = "your-service-account@your-project.iam.gserviceaccount.com"
-# client_id = "your-client-id"
-# auth_uri = "https://accounts.google.com/o/oauth2/auth"
-# token_uri = "https://oauth2.googleapis.com/token"
-# auth_provider_x509_cert_url = "https://www.googleapis.com/oauth2/v1/certs"
-# client_x509_cert_url = "https://www.googleapis.com/robot/v1/metadata/x509/your-service-account%40your-project.iam.gserviceaccount.com"
-# #private_key 부분에서 줄바꿈 (\n)을 꼭 \\n 으로 이스케이프해야 오류가 안 나요!
-
-
-import streamlit as st
 from datetime import datetime #pip install streamlit-datetime-picker
 import pandas as pd #pip install pandas
-from supabase import create_client, Client #pip install streamlit supabase 설치하기
+from supabase import create_client, Client #pip install streamlit supabase
 
-# Supabase 프로젝트 URL과 API 키 입력
-SUPABASE_URL = "https://ednbuvtckoahmtkcrlkw.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkbmJ1dnRja29haG10a2NybGt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQxMDk2NDIsImV4cCI6MjA1OTY4NTY0Mn0.OAe8fAKYfHXp5gCL8-7UMHQZI2StBYEYJqfGjzMLv2o"
+# Supabase 연결
+url = st.secrets["supabase"]["url"]
+key = st.secrets["supabase"]["key"]
+supabase: Client = create_client(url, key)
 
 # Supabase 클라이언트 생성
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+#supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 today=datetime.now().strftime("%Y%m%d")
 #st.write(today)
 
-#세션당 1번만 기록
+#세션당 1번씩 기록
 # if "visited" not in st.session_state:
 #     st.session_state.visited=True
 #     supabase.table("mmaconn").insert ({"date":today}).execute()
