82387c248e14e4daa2f6a7a45ad34d428b6a7925
jennyheo <hyj3589@gmail.com>
2025-06-20 13:55:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ key = st.secrets["supabase"]["key"]
 supabase: Client = create_client(url, key)
 
 # UI 예시
-st.subheader('🚌 동원훈련 버스정보 조회')
-name = st.text_input("이름").replace(" ", "")  # 공백 제거
-phone = st.text_input("전화번호 뒷자리 (숫자 4자리)", max_chars=4)
-
+st.title("버스번호 조회")
+name = st.text_input("이름")
+phone = st.text_input("전화번호 뒷자리 (4자리)", max_chars=4)
 
 if st.button("조회하기"):
     if not name or not phone:
-        st.warning("이름과 전화번호를 모두 입력해주세요.")
+        st.error("이름과 전화번호 뒷자리를 모두 입력해주세요.")
     else:
-        res = supabase.table("businfo").select("busno").eq("name", name).eq("phone", phone).execute()
+        res = supabase.table("businfo").select("busno, irno").eq("name", name).eq("phone", phone).execute()
+        irno = res.data[0]["irno"]
         if res.data:
-            st.success(f"{name}님께서 탑승하실 버스는 **{res.data[0]['busno']} 번 버스**입니다.")
+            st.success(f"당신의 버스번호는: {res.data[0]['busno']} irno")
+
         else:
             st.warning("일치하는 정보가 없습니다.")
-
 
