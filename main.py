c65adef98dfcd312cf8cf050edb083ae0f097312
jennyheo <hyj3589@gmail.com>
2025-06-23 16:54:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ if 'kkk1' not in st.session_state or st.session_state['kkk1'] is None:
     phone = st.text_input("전화번호 뒷자리 (4자리)", max_chars=4)
     if st.button("조회하기"):
         st.session_state['kkk1'] = ''
-        st.write(name, phone) 
+        st.session_state['kkk2'] = ''
+        #st.write(name, phone) 
         if not name or not phone:
             st.error("이름과 전화번호 뒷자리를 모두 입력해주세요.")
         else:
@@ -31,7 +32,8 @@ if 'kkk1' not in st.session_state or st.session_state['kkk1'] is None:
                 st.success(f"{name}님께서 승차하실 버스번호는 {busno} 입니다.")
                 if st.button("비상연락처 입력하기"):
                     st.session_state['kkk1'] = irno
-                    st.write(st.session_state['kkk1'])
+                    st.session_state['kkk2'] = name
+                    #st.write(st.session_state['kkk1'])
                     #st.rerun() #리런한다
             else:
                 st.warning("일치하는 정보가 없습니다.")
