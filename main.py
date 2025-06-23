e473d955b4a8cc917b4f72655ef85a8addf8397a
jennyheo <hyj3589@gmail.com>
2025-06-23 17:13:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -49,6 +49,8 @@ else:
                 if st.button("비상연락처 입력하기"):
                     st.session_state['kkk1'] = irno
                     st.session_state['kkk2'] = name
+                    st.write(st.session_state['kkk1'])
+                    st.write(st.session_state['kkk2'])
                     #st.rerun() #리런한다
             else:
                 st.warning("일치하는 정보가 없습니다.")
