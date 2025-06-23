6a9eb48d4d156ed0515b1bda104c1aa97d2cc4a3
jennyheo <hyj3589@gmail.com>
2025-06-23 16:18:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ if st.button("조회하기"):
             st.success(f"{name}님께서 승차하실 버스번호는 {busno} 입니다.")
             if st.button("비상연락처 입력하기"):
                 st.session_state['kkk1'] = irno
-                #st.write(st.session_state['kkk1'])
+                st.write(st.session_state['kkk1'])
                 #st.rerun() #리런한다
         else:
             st.warning("일치하는 정보가 없습니다.")
