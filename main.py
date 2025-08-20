f5368fc18801affa57305ba13f0c7e4db4fad92d
jennyheo <hyj3589@gmail.com>
2025-08-20 17:16:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ phone = st.text_input("전화번호 뒷자리 (4자리)", max_chars=4)
 if st.button("버스정보 조회하기"):
     if not name or not phone:
         st.error("이름과 전화번호 뒷자리를 모두 입력해주세요.")
+    elif not phone.isdigit():
+        st.error("전화번호 뒷자리는 숫자만 입력 가능합니다.")
     elif len(phone) != 4:
         st.error("전화번호 뒷자리는 4자리로 입력해주세요.")
     else:
