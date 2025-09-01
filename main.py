9710fa5bc9e4e59137b001064c79ec7b9ac19f21
jennyheo <hyj3589@gmail.com>
2025-09-01 11:04:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,22 @@ phone = st.text_input("전화번호 뒷자리 (4자리)", max_chars=4)
 if st.button("버스정보 조회하기"):
     if not name or not phone:
         st.error("이름과 전화번호 뒷자리를 모두 입력해주세요.")
+        first_char = name[0]
+        third_char = name[2]
     elif not phone.isdigit():
         st.error("전화번호 뒷자리는 숫자만 입력 가능합니다.")
     elif len(phone) != 4:
         st.error("전화번호 뒷자리는 4자리로 입력해주세요.")
     else:
-        res = supabase.table("businfo").select("busno, irno").eq("name", name).eq("phone", phone).execute()
+        phone2=phone[-3:]
+        #res = supabase.table("businfo").select("busno, irno").eq("name", name).eq("phone", phone).execute()
+        
+        res = (supabase.table("businfo")
+           .select("*")
+           .like("name", f"{first_char}_{third_char}%")
+           .eq("phone", phone3)
+           .execute())
+        
         if res.data:
             busno = res.data[0]['busno']
             irno = res.data[0]['irno']
