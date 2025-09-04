03763f1529aea0b5bb78cad2281e88a60ab481d4
jennyheo <hyj3589@gmail.com>
2025-09-04 10:30:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ if st.button("버스정보 조회하기"):
                         {name}님께서 승차하실 버스번호는
                     </span><br>
                     <span style='font-size:35px; font-weight:bold; color:#0b5394;'>
-                        {busno} ~ 91번
+                        {busno}
                     </span>
                     <span style='font-size:16px; color:#155724;'>
                         입니다.
